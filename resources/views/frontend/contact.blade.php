ffbaf0f972add06b5a35e14c5950202845417533
chukwudi79 <okoriechukwudi4@gmail.com>
2019-08-08 06:10:07 +0100

    --- Auto-Git Commit ---

--- resources/views/frontend/contact.blade.php
+++ resources/views/frontend/contact.blade.php
@@ -1,11 +1,11 @@
 @extends('frontend.layout.main')
 
 @section('content')
-<div class="hero-wrap hero-bread" style="background-image: url('images/bg_1.jpg');">
+<div class="hero-wrap hero-bread" style="background-image: url({{asset('bootstrap_assets/images/bg_1.jpg')}});">
     <div class="container">
       <div class="row no-gutters slider-text align-items-center justify-content-center">
         <div class="col-md-9 ftco-animate text-center">
-            <p class="breadcrumbs"><span class="mr-2"><a href="index.html">Home</a></span> <span>Contact us</span></p>
+            <p class="breadcrumbs"><span class="mr-2"><a href="{{route('index')}}">Home</a></span> <span>Contact us</span></p>
           <h1 class="mb-0 bread">Contact us</h1>
         </div>
       </div>
