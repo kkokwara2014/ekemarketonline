c84b008c1b789df0163ae1058dcc9fdefc22cd70
kkokwara2014 <kkokwara2014@gmail.com>
2019-09-06 11:52:04 +0100

    --- Auto-Git Commit ---

--- resources/views/admin/layout/header.blade.php
+++ resources/views/admin/layout/header.blade.php
@@ -3,35 +3,35 @@
   <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title>IFMS | Dashboard</title>
+    <title>Ekemarketonline | Dashboard</title>
     <!-- Tell the browser to be responsive to screen width -->
     <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
     <!-- Bootstrap 3.3.7 -->
-    <link rel="stylesheet" href="{{asset('bootstrap_assets/bower_components/bootstrap/dist/css/bootstrap.min.css')}}">
+    <link rel="stylesheet" href="{{asset('admin_assets/bower_components/bootstrap/dist/css/bootstrap.min.css')}}">
     <!-- Font Awesome -->
-    <link rel="stylesheet" href="{{asset('bootstrap_assets/bower_components/font-awesome/css/font-awesome.min.css')}}">
+    <link rel="stylesheet" href="{{asset('admin_assets/bower_components/font-awesome/css/font-awesome.min.css')}}">
     <!-- Ionicons -->
-    <link rel="stylesheet" href="{{asset('bootstrap_assets/bower_components/Ionicons/css/ionicons.min.css')}}">
+    <link rel="stylesheet" href="{{asset('admin_assets/bower_components/Ionicons/css/ionicons.min.css')}}">
     <!-- Theme style -->
-    <link rel="stylesheet" href="{{asset('bootstrap_assets/dist/css/AdminLTE.min.css')}}">
+    <link rel="stylesheet" href="{{asset('admin_assets/dist/css/AdminLTE.min.css')}}">
     <!-- AdminLTE Skins. Choose a skin from the css/skins
        folder instead of downloading all of them to reduce the load. -->
-    <link rel="stylesheet" href="{{asset('bootstrap_assets/dist/css/skins/_all-skins.min.css')}}">
+    <link rel="stylesheet" href="{{asset('admin_assets/dist/css/skins/_all-skins.min.css')}}">
     <!-- Morris chart -->
-    <link rel="stylesheet" href="{{asset('bootstrap_assets/bower_components/morris.js/morris.css')}}">
+    <link rel="stylesheet" href="{{asset('admin_assets/bower_components/morris.js/morris.css')}}">
     <!-- jvectormap -->
-    <link rel="stylesheet" href="{{asset('bootstrap_assets/bower_components/jvectormap/jquery-jvectormap.css')}}">
+    <link rel="stylesheet" href="{{asset('admin_assets/bower_components/jvectormap/jquery-jvectormap.css')}}">
     <!-- Date Picker -->
     <link rel="stylesheet"
-      href="{{asset('bootstrap_assets/bower_components/bootstrap-datepicker/dist/css/bootstrap-datepicker.min.css')}}">
+      href="{{asset('admin_assets/bower_components/bootstrap-datepicker/dist/css/bootstrap-datepicker.min.css')}}">
     <!-- Daterange picker -->
     <link rel="stylesheet"
-      href="{{asset('bootstrap_assets/bower_components/bootstrap-daterangepicker/daterangepicker.css')}}">
+      href="{{asset('admin_assets/bower_components/bootstrap-daterangepicker/daterangepicker.css')}}">
     <!-- bootstrap wysihtml5 - text editor -->
     <link rel="stylesheet"
-      href="{{asset('bootstrap_assets/plugins/bootstrap-wysihtml5/bootstrap3-wysihtml5.min.css')}}">
+      href="{{asset('admin_assets/plugins/bootstrap-wysihtml5/bootstrap3-wysihtml5.min.css')}}">
         <!-- DataTables -->
-  <link rel="stylesheet" href="{{asset('bootstrap_assets/bower_components/datatables.net-bs/css/dataTables.bootstrap.min.css')}}">
+  <link rel="stylesheet" href="{{asset('admin_assets/bower_components/datatables.net-bs/css/dataTables.bootstrap.min.css')}}">
 
     <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
     <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
@@ -54,7 +54,7 @@
           <!-- mini logo for sidebar mini 50x50 pixels -->
           <span class="logo-mini"><b>A</b>LT</span>
           <!-- logo for regular state and mobile devices -->
-          <span class="logo-lg"><b>IFMS</b> Dashboard</span>
+          <span class="logo-lg"><b>Ekemarketonline</b> Dashboard</span>
         </a>
         <!-- Header Navbar: style can be found in header.less -->
         <nav class="navbar navbar-static-top">
