5d1520c8fe99b19dea2ea859dbb2ec68e45d3fc0
kkokwara2014 <kkokwara2014@gmail.com>
2019-09-06 11:49:30 +0100

    Adjusting the admin panel

--- resources/views/admin/layout/header.blade.php
+++ resources/views/admin/layout/header.blade.php
@@ -1,35 +1,37 @@
 <!DOCTYPE html>
 <html>
-
   <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title>Admin | Dashboard</title>
+    <title>IFMS | Dashboard</title>
     <!-- Tell the browser to be responsive to screen width -->
     <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
     <!-- Bootstrap 3.3.7 -->
-    <link rel="stylesheet" href="{{asset('admin_assets/bower_components/bootstrap/dist/css/bootstrap.min.css')}}">
+    <link rel="stylesheet" href="{{asset('bootstrap_assets/bower_components/bootstrap/dist/css/bootstrap.min.css')}}">
     <!-- Font Awesome -->
-    <link rel="stylesheet" href="{{asset('admin_assets/bower_components/font-awesome/css/font-awesome.min.css')}}">
+    <link rel="stylesheet" href="{{asset('bootstrap_assets/bower_components/font-awesome/css/font-awesome.min.css')}}">
     <!-- Ionicons -->
-    <link rel="stylesheet" href="{{asset('admin_assets/bower_components/Ionicons/css/ionicons.min.css')}}">
+    <link rel="stylesheet" href="{{asset('bootstrap_assets/bower_components/Ionicons/css/ionicons.min.css')}}">
     <!-- Theme style -->
-    <link rel="stylesheet" href="{{asset('admin_assets/dist/css/AdminLTE.min.css')}}">
+    <link rel="stylesheet" href="{{asset('bootstrap_assets/dist/css/AdminLTE.min.css')}}">
     <!-- AdminLTE Skins. Choose a skin from the css/skins
        folder instead of downloading all of them to reduce the load. -->
-    <link rel="stylesheet" href="{{asset('admin_assets/dist/css/skins/_all-skins.min.css')}}">
+    <link rel="stylesheet" href="{{asset('bootstrap_assets/dist/css/skins/_all-skins.min.css')}}">
     <!-- Morris chart -->
-    <link rel="stylesheet" href="{{asset('admin_assets/bower_components/morris.js/morris.css')}}">
+    <link rel="stylesheet" href="{{asset('bootstrap_assets/bower_components/morris.js/morris.css')}}">
     <!-- jvectormap -->
-    <link rel="stylesheet" href="{{asset('admin_assets/bower_components/jvectormap/jquery-jvectormap.css')}}">
+    <link rel="stylesheet" href="{{asset('bootstrap_assets/bower_components/jvectormap/jquery-jvectormap.css')}}">
     <!-- Date Picker -->
     <link rel="stylesheet"
-      href="{{asset('admin_assets/bower_components/bootstrap-datepicker/dist/css/bootstrap-datepicker.min.css')}}">
+      href="{{asset('bootstrap_assets/bower_components/bootstrap-datepicker/dist/css/bootstrap-datepicker.min.css')}}">
     <!-- Daterange picker -->
     <link rel="stylesheet"
-      href="{{asset('admin_assets/bower_components/bootstrap-daterangepicker/daterangepicker.css')}}">
+      href="{{asset('bootstrap_assets/bower_components/bootstrap-daterangepicker/daterangepicker.css')}}">
     <!-- bootstrap wysihtml5 - text editor -->
-    <link rel="stylesheet" href="{{asset('admin_assets/plugins/bootstrap-wysihtml5/bootstrap3-wysihtml5.min.css')}}">
+    <link rel="stylesheet"
+      href="{{asset('bootstrap_assets/plugins/bootstrap-wysihtml5/bootstrap3-wysihtml5.min.css')}}">
+        <!-- DataTables -->
+  <link rel="stylesheet" href="{{asset('bootstrap_assets/bower_components/datatables.net-bs/css/dataTables.bootstrap.min.css')}}">
 
     <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
     <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
@@ -52,7 +54,7 @@
           <!-- mini logo for sidebar mini 50x50 pixels -->
           <span class="logo-mini"><b>A</b>LT</span>
           <!-- logo for regular state and mobile devices -->
-          <span class="logo-lg"><b>Ekemarket</b>Online</span>
+          <span class="logo-lg"><b>IFMS</b> Dashboard</span>
         </a>
         <!-- Header Navbar: style can be found in header.less -->
         <nav class="navbar navbar-static-top">
@@ -67,22 +69,22 @@
               <!-- User Account: style can be found in dropdown.less -->
               <li class="dropdown user user-menu">
                 <a href="#" class="dropdown-toggle" data-toggle="dropdown">
-                  <img src="{{asset('admin_assets/dist/img/user2-160x160.jpg')}}" class="user-image" alt="User Image">
-                  <span class="hidden-xs">{{ ucfirst(Auth::user()->name) }}</span>
+                  <img src="{{asset('bootstrap_assets/dist/img/user2-160x160.jpg')}}" class="user-image"
+                    alt="User Image">
+                  <span class="hidden-xs">{{Auth::user()->name}}</span>
                 </a>
                 <ul class="dropdown-menu">
                   <!-- User image -->
                   <li class="user-header">
-                    <img src="{{asset('admin_assets/dist/img/user2-160x160.jpg')}}" class="img-circle" alt="User Image">
+                    <img src="{{asset('bootstrap_assets/dist/img/user2-160x160.jpg')}}" class="img-circle"
+                      alt="User Image">
 
                     <p>
-                      {{ ucfirst(Auth::user()->name) }} - Admin
-                      <small>Member since {{Auth::user()->created_at->format('d M Y')}}</small>
+                      {{Auth::user()->name}} - {{Auth::user()->role->name}}
+                      <small>Member since {{Auth::user()->created_at->diffForHumans()}}</small>
                     </p>
                   </li>
-                  <!-- Menu Body -->
-                  <li class="user-body">
-                  </li>
+
                   <!-- Menu Footer-->
                   <li class="user-footer">
                     <div class="pull-left">
@@ -94,25 +96,7 @@
                   </li>
                 </ul>
               </li>
-              {{-- <!-- Control Sidebar Toggle Button -->
-              <li>
-                <a href="#" data-toggle="control-sidebar"><i class="fa fa-gears"></i></a>
-              </li> --}}
             </ul>
           </div>
         </nav>
       </header>
-
-      <!-- Content Wrapper. Contains page content -->
-      <div class="content-wrapper">
-        <!-- Content Header (Page header) -->
-        <section class="content-header">
-          <h1>
-            {{ $pageTitle }}
-            <small>Control panel</small>
-          </h1>
-          {{-- <ol class="breadcrumb">
-            <li><a href="#"><i class="fa fa-dashboard"></i> Home</a></li>
-            <li class="active">Dashboard</li>
-          </ol> --}}
-        </section>
---
+++ resources/views/admin/layout/sidebar.blade.php
@@ -0,0 +1,152 @@
+<!-- Left side column. contains the logo and sidebar -->
+<aside class="main-sidebar">
+  <!-- sidebar: style can be found in sidebar.less -->
+  <section class="sidebar">
+    <!-- Sidebar user panel -->
+    <div class="user-panel">
+      <div class="pull-left image">
+        <img src="{{asset('bootstrap_assets/dist/img/user2-160x160.jpg')}}" class="img-circle" alt="User Image">
+      </div>
+      <div class="pull-left info">
+        <p>{{Auth::user()->name}}</p>
+        <a href="#"><i class="fa fa-circle text-success"></i> Online</a>
+      </div>
+    </div>
+
+    <!-- sidebar menu: : style can be found in sidebar.less -->
+    <ul class="sidebar-menu" data-widget="tree">
+
+      <li class="active treeview">
+        <a href="#">
+          <i class="fa fa-dashboard"></i> <span>Dashboard</span>
+          <span class="pull-right-container">
+            {{-- <i class="fa fa-angle-left pull-right"></i> --}}
+          </span>
+        </a>
+
+      </li>
+
+
+      <li class="treeview">
+        <a href="#">
+          <i class="fa fa-edit"></i>
+          <span>Accounts</span>
+          <span class="pull-right-container">
+            <i class="fa fa-angle-left pull-right"></i>
+          </span>
+        </a>
+        <ul class="treeview-menu">
+          <li><a href="#"><i class="fa fa-circle-o"></i> Payable</a></li>
+          <li><a href="#"><i class="fa fa-circle-o"></i> Receivable</a></li>
+        </ul>
+      </li>
+
+      <li class="treeview">
+        <a href="#">
+          <i class="fa fa-edit"></i>
+          <span>Finance</span>
+          <span class="pull-right-container">
+            <i class="fa fa-angle-left pull-right"></i>
+          </span>
+        </a>
+        <ul class="treeview-menu">
+          <li><a href="#"><i class="fa fa-circle-o"></i> Budget</a></li>
+          <li><a href="#"><i class="fa fa-circle-o"></i> Fund Retirement</a></li>
+        </ul>
+      </li>
+
+      <li class="treeview">
+        <a href="#">
+          <i class="fa fa-edit"></i>
+          <span>Personnel</span>
+          <span class="pull-right-container">
+            <i class="fa fa-angle-left pull-right"></i>
+          </span>
+        </a>
+        <ul class="treeview-menu">
+        <li><a href="{{ route('mda.index') }}"><i class="fa fa-circle-o"></i> MDA</a></li>
+        <li><a href="{{ route('department.index') }}"><i class="fa fa-circle-o"></i> Department</a></li>
+          <li><a href="#"><i class="fa fa-circle-o"></i> Employee</a></li>
+          <li><a href="#"><i class="fa fa-circle-o"></i> Appraisal</a></li>
+        </ul>
+      </li>
+
+      <li class="treeview">
+        <a href="#">
+          <i class="fa fa-edit"></i>
+          <span>Procurement</span>
+          <span class="pull-right-container">
+            <i class="fa fa-angle-left pull-right"></i>
+          </span>
+        </a>
+        <ul class="treeview-menu">
+          <li><a href="#"><i class="fa fa-circle-o"></i> Assets</a></li>
+          <li><a href="#"><i class="fa fa-circle-o"></i> Order</a></li>
+          <li><a href="#"><i class="fa fa-circle-o"></i> Stock</a></li>
+        <li><a href="{{ route('inventory.index') }}"><i class="fa fa-circle-o"></i> Inventory</a></li>
+        </ul>
+      </li>
+
+      <li class="treeview">
+        <a href="#">
+          <i class="fa fa-edit"></i>
+          <span>Expenditure</span>
+          <span class="pull-right-container">
+            <i class="fa fa-angle-left pull-right"></i>
+          </span>
+        </a>
+        <ul class="treeview-menu">
+          <li><a href="#"><i class="fa fa-circle-o"></i> Employee</a></li>
+          <li><a href="#"><i class="fa fa-circle-o"></i> Morris</a></li>
+          <li><a href="#"><i class="fa fa-circle-o"></i> Flot</a></li>
+          <li><a href="#"><i class="fa fa-circle-o"></i> Inline charts</a></li>
+        </ul>
+      </li>
+
+      <li class="treeview">
+        <a href="#">
+          <i class="fa fa-edit"></i>
+          <span>Revenue</span>
+          <span class="pull-right-container">
+            <i class="fa fa-angle-left pull-right"></i>
+          </span>
+        </a>
+        <ul class="treeview-menu">
+          <li><a href="#"><i class="fa fa-circle-o"></i> Employee</a></li>
+          <li><a href="#"><i class="fa fa-circle-o"></i> Morris</a></li>
+          <li><a href="#"><i class="fa fa-circle-o"></i> Flot</a></li>
+          <li><a href="#"><i class="fa fa-circle-o"></i> Inline charts</a></li>
+        </ul>
+      </li>
+
+      <li class="treeview">
+        <a href="#">
+          <i class="fa fa-edit"></i> <span>Advances</span>
+          <span class="pull-right-container">
+            <i class="fa fa-angle-left pull-right"></i>
+          </span>
+        </a>
+        <ul class="treeview-menu">
+          <li><a href="#"><i class="fa fa-circle-o"></i> Employee Advance</a></li>
+          <li><a href="#"><i class="fa fa-circle-o"></i> Order Advance</a></li>
+          <li><a href="#"><i class="fa fa-circle-o"></i> Tour Advance</a></li>
+        </ul>
+      </li>
+
+      <li class="treeview">
+        <a href="#">
+          <i class="fa fa-edit"></i> <span>Third Party</span>
+          <span class="pull-right-container">
+            <i class="fa fa-angle-left pull-right"></i>
+          </span>
+        </a>
+        <ul class="treeview-menu">
+        <li><a href="{{ route('contractor.index') }}"><i class="fa fa-circle-o"></i> Contractor</a></li>
+          <li><a href="{{ route('supplier.index') }}"><i class="fa fa-circle-o"></i> Supplier</a></li>
+        </ul>
+      </li>
+
+    </ul>
+  </section>
+  <!-- /.sidebar -->
+</aside>
