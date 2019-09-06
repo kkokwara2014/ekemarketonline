8b510936ac8b763a7037c83007b0f008e3da180c
kkokwara2014 <kkokwara2014@gmail.com>
2019-09-06 10:50:51 +0100

    Update navbar.blade.php

--- resources/views/frontend/layout/navbar.blade.php
+++ resources/views/frontend/layout/navbar.blade.php
@@ -58,10 +58,7 @@
         </li>
         <li class="nav-item"><a href="#" class="nav-link">About</a></li>
         <li class="nav-item"><a href="#" class="nav-link">Contact</a></li>
-        {{-- <li class="nav-item cta cta-colored"><a href="#" class="nav-link"><span
-              class="icon-shopping_cart"></span>[0]</a>
-        </li> --}}
-
+        
       </ul>
     </div>
   </div>
