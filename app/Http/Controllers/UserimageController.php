e1a51c785c40a13bbce81327e6e7a28182d3cf83
kkokwara2014 <kkokwara2014@gmail.com>
2019-09-07 03:27:42 +0100

    --- Auto-Git Commit ---

--- app/Http/Controllers/UserimageController.php
+++ app/Http/Controllers/UserimageController.php
@@ -23,7 +23,7 @@ class UserimageController extends Controller
      */
     public function create()
     {
-        
+        return view('admin.userimage.create');
     }
 
     /**
--- resources/views/admin/userimage/create.blade.php
+++ resources/views/admin/userimage/create.blade.php
@@ -11,11 +11,32 @@
 
         <br>
         <div class="row">
-            <div class="col-md-6">
-                <img src="" alt="">
-                <form action="" method="post" enctype="multipart/form-data">
+            <div class="col-md-5">
+                <div class="box">
+                    <!-- /.box-header -->
+                    <div class="box-body">
+                        <div class="row">
+                            <div class="col-md-3"></div>
+                            <div class="col-md-6">
+                                <img src="{{url('user_images','no_user.jpg')}}" alt="" class="img-responsive img-circle"
+                                    width="250" height="250">
+                                <form action="" method="post" enctype="multipart/form-data">
+                                    <input type="hidden" name="user_id" value="{{Auth::user()->id}}">
+                                    <br>
+                                    <input type="file" name="imagename">
+                                    <p></p>
+                                    <button type="submit" class="btn btn-success text-center"><span class="fa fa-upload"></span>
+                                        Upload your
+                                        Photo</button>
+                                </form>
+                            </div>
+                            <div class="col-md-3"></div>
+                        </div>
 
-                </form>
+                    </div>
+                    <!-- /.box-body -->
+                </div>
+                <!-- /.box -->
             </div>
         </div>
 
