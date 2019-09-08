a308ba39b2f79e833f6801a468cfca314b0df1a0
kkokwara2014 <kkokwara2014@gmail.com>
2019-09-08 04:58:56 +0100

    --- Auto-Git Commit ---

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -6,7 +6,7 @@ use App\Category;
 use App\Http\Controllers\Controller;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
 
-use App\Http\Controllers\Auth\Request;
+use \Illuminate\Http\Request;
 
 class LoginController extends Controller
 {
