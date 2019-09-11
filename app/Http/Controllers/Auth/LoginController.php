0b3b68a8c014e9a5c194fc268a7530844630ea28
kkokwara2014 <kkokwara2014@gmail.com>
2019-09-11 15:29:51 +0100

    --- Auto-Git Commit ---

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -8,6 +8,8 @@ use Illuminate\Foundation\Auth\AuthenticatesUsers;
 
 use \Illuminate\Http\Request;
 
+use App\User;
+
 class LoginController extends Controller
 {
     /*
@@ -37,7 +39,7 @@ class LoginController extends Controller
      */
     public function __construct()
     {
-        $this->middleware('guest')->except(['logout','userLogout']);
+        $this->middleware('guest')->except(['logout', 'userLogout']);
     }
 
     public function showLoginForm()
@@ -66,21 +68,26 @@ class LoginController extends Controller
     {
         $errors = [$this->username() => trans('auth.failed')];
 
+
         // Load user from database
         $user = User::where($this->username(), $request->{$this->username()})->first();
 
         // Check if user was successfully loaded, that the password matches
         // and active is not 1. If so, override the default error message.
-        if ($user && \Hash::check($request->password, $user->password) && $user->active != 1) {
-            $errors = [$this->username() => trans('auth.notactivated')];
+        if ($user && \Hash::check($request->password, $user->password) && $user->isactive != 1) {
+            // $errors = [$this->username() => trans('auth.notactivated')];
+            // $errors = trans('auth.notactivated');
+            return redirect()->back()->with('error', 'Your account has not been activated yet! Contact Admin.')
+                ->withInput($request->only($this->username(), 'remember'));
+                // ->withErrors($errors)
         }
 
-        if ($request->expectsJson()) {
-            return response()->json($errors, 422);
-        }
-        return redirect()->back()
-            ->withInput($request->only($this->username(), 'remember'))
-            ->withErrors($errors);
+        // if ($request->expectsJson()) {
+        //     return response()->json($errors, 422);
+        // }
+        // return redirect()->back()
+        //     ->withInput($request->only($this->username(), 'remember'))
+        //     ->withErrors($errors);
     }
 
 
